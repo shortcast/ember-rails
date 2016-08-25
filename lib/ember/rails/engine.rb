5748ade32b85501700d8e29614fbb48567297e9f
zumbalogy <intrapostal@gmail.com>
2016-08-25 17:57:29 -0400

    enabling transfomers

--- lib/ember/rails/engine.rb
+++ lib/ember/rails/engine.rb
@@ -14,12 +14,12 @@ module Ember
 
       config.before_initialize do |app|
         Sprockets::Engines #force autoloading
-        # Sprockets.register_mime_type '.handlebars'
-        # Sprockets.register_mime_type '.hbs'
-        # Sprockets.register_mime_type '.hjs'
-        # Sprockets.register_transformer '.hbs', 'application/javascript', Ember::Handlebars::Template
-        # Sprockets.register_transformer '.hjs', 'application/javascript', Ember::Handlebars::Template
-        # Sprockets.register_transformer '.handlebars', 'application/javascript', Ember::Handlebars::Template
+        Sprockets.register_mime_type '.handlebars'
+        Sprockets.register_mime_type '.hbs'
+        Sprockets.register_mime_type '.hjs'
+        Sprockets.register_transformer '.hbs', 'application/javascript', Ember::Handlebars::Template
+        Sprockets.register_transformer '.hjs', 'application/javascript', Ember::Handlebars::Template
+        Sprockets.register_transformer '.handlebars', 'application/javascript', Ember::Handlebars::Template
       end
     end
   end
